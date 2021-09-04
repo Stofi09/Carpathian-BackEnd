abd1d91907d1ec4fa114b657cbf99423043a73bd
Kris <kristof.solak@gmail.com>
2021-09-04 13:05:42 +0100

    Cors added for development

--- src/main/java/com/carpathian/controller/APIController.java
+++ src/main/java/com/carpathian/controller/APIController.java
@@ -1,12 +1,14 @@
 package com.carpathian.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.carpathian.domain.Food;
 import com.carpathian.service.FoodService;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class APIController {
 
@@ -18,5 +20,8 @@ public class APIController {
 		return foodService.getAllFood();
 	}
 	
-	
+	@RequestMapping("/getAllFood")
+	public List<Food> getAllFood() {
+		return foodService.getAllFood();
+	}
 }
