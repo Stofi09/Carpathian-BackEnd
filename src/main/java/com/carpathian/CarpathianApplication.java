eeabb88b39fafcc9df4ff98dab7645256e57a9c6
Kris <kristof.solak@gmail.com>
2021-09-04 11:24:41 +0100

    first commit

---
+++ src/main/java/com/carpathian/CarpathianApplication.java
@@ -0,0 +1,13 @@
+package com.carpathian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpathianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpathianApplication.class, args);
+	}
+
+}
